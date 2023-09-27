67d9fda905d95d30ec891b1cf7ee650d93905305
MrBs <Chouaibwidadipur@gmail.com>
2023-09-27 19:03:53 +0100

    Fixed Border radius

--- components/BackGround.tsx
+++ components/BackGround.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function BackGround() {
   return (
-    <div className="absolute top-0 left-0 w-full h-full max-h-[500px] bg-Secondary rounded-3xl overflow-hidden">
+    <div className="absolute top-0 left-0 w-full h-full max-h-[500px] bg-Secondary rounded-ee-3xl rounded-es-3xl  overflow-hidden">
       <div className="w-20 aspect-square rounded-full bg-Main -top-6 right-1/3 absolute"></div>
       <div className="w-40 aspect-square rounded-full bg-Main -top-10 left-10 absolute"></div>
       <div className="w-32 aspect-square rounded-full bg-Main top-1/2 -left-10 absolute"></div>
